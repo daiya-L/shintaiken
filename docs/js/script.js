22922c241bb981262d2b1738e1505879ef071b9a
daiya-elu <tenbeyjun@icloud.com>
2019-11-06 16:08:13 +0900

    add html css js

---
+++ docs/js/script.js
@@ -0,0 +1,7 @@
+$(function(){
+    $('.main-content-wrapper').stickyStack({
+        containerElement: '.main-content-wrapper',
+        stackingElement: 'section',
+        boxShadow: '0 -3px 20px rgba(0, 0, 0, 0.25)'
+    });
+})
