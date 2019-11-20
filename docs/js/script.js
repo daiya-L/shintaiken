e9478d69a44cbbc75866c8cd937285b78c263bfb
daiya-L <52106321+daiya-L@users.noreply.github.com>
2019-11-20 15:45:18 +0900

    fixed vh

--- docs/js/script.js
+++ docs/js/script.js
@@ -11,14 +11,14 @@ $(function(){
     return false;
   });
 });*/
-
+/*
 $(function(){
     $('.main-content-wrapper').stickyStack({
         containerElement: '.main-content-wrapper',
         stackingElement: 'section',
         boxShadow: '0 -3px 20px rgba(0, 0, 0, 0.25)'
     });
-});
+});*/
 
 $(function(){
   $('#about__link').click(function(){
